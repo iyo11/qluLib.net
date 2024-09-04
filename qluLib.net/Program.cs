5e97a17e1540866948585913f598a32b1051ce01
iyo11 <IYO.MAIL.CN@gmail.com>
2024-09-04 21:05:37 +0800

    [Feat] > Auto Reserve

--- qluLib.net/Program.cs
+++ qluLib.net/Program.cs
@@ -2,24 +2,39 @@
 using qluLib.net.Lib;
 using qluLib.net.Sso;
 using qluLib.net.Url;
+//你的学号
+const string userName = "";
+//你的sso密码
+const string passWord = "";
+const Area area = Area.六楼东;
+const AreaTime areaTime = AreaTime.Tomorrow;
+const SeatId seatId = SeatId.六楼东001;
 
-namespace qluLib.net;
+var sso = new SSO();
+var library = new Library();
+var url = new QluLibUrl();
+List<string> cookies =[];
+var pastHour = 0;
 
-internal static class Program
+while (true)
 {
-    private static async Task Main(string[] args)
+    DateTime now = DateTime.Now;
+    if (now.Hour == 21 && now.Minute == 30 && now.Second == 0)
     {
-        var sso = new SSO();
-        
-        var library = new Library();
-        var url = new QluLibUrl();
-        
-        const string userName = "你的学号";
-        const string passWord = "你的密码";
-        
-        var cookies = await sso.GetCookies(userName,passWord,url);
-        await library.Reserve(url, cookies, AreaTime.Tomorrow, Area.六楼东, SeatId.六楼东001);
+        cookies = (await sso.GetCookies(userName, passWord, url)).ToList();
     }
-    
-    
+    if (now.Hour == 22 && now.Minute == 00 && now.Second == 0){
+        if (cookies.Count == 0)
+        {
+            cookies = (await sso.GetCookies(userName, passWord, url)).ToList();
+        }
+        await library.Reserve(url, cookies, areaTime, area, seatId);
+        cookies.Clear();
+    }
+    if(now.Hour != pastHour)
+    {
+        pastHour = now.Hour;
+        Console.WriteLine($"[{now}] {userName} -> {areaTime} > {area} > {seatId}");
+    }
+    Task.Delay(500).Wait();
 }
