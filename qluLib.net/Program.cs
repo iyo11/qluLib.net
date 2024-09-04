2cda0d6f76b52a8e900838bf8eb2b2bdacc11861
iyo11 <IYO.MAIL.CN@gmail.com>
2024-09-04 16:56:47 +0800

    Initial commit

--- qluLib.net/Program.cs
+++ qluLib.net/Program.cs
@@ -17,14 +17,7 @@ internal static class Program
         const string userName = "你的学号";
         const string passWord = "你的密码";
         
-        var cookies = (await sso.GetCookies(userName,passWord,url)).ToList();
-        
-        Console.WriteLine($"获取到Cookies:");
-        foreach (var cookie in cookies)
-        {
-            Console.WriteLine(cookie);
-        }
-
+        var cookies = await sso.GetCookies(userName,passWord,url);
         await library.Reserve(url, cookies, AreaTime.Tomorrow, Area.六楼东, SeatId.六楼东001);
     }
     
